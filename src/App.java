3cfac2e5ce9b00e780b386de2023b8b80d345ba9
juanfra <jruirom0311@g.educaand.es>
2025-11-06 20:45:14 +0100

    Tercer commit con Scanner

--- src/App.java
+++ src/App.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hello, World!");
         System.out.println("hola que haces");
         System.out.println("hola que haces");
-
+        System.out.println("Tercero");
     }
 }
